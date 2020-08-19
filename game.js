f3bf8266ecd37ec4e9f0140a10d0d837bf5a2a3f
Glitch (hello-webpage) <none>
2020-08-19 01:18:14 +0000

    💘🎃 Checkpoint ./game.js:11505919/17659

--- game.js
+++ game.js
@@ -24,8 +24,8 @@ const config = {
 const game = new Phaser.Game(config);
 
   function preload() { 
-  this.load.image('tiles', 'https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2Fspritesheet_ground.png?v=1597798791918);
-  this.load.image('tiles', 'https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2Fspritesheet_tiles.png?v=1597798793579);
+  this.load.image('ground', 'https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2Fspritesheet_ground.png?v=1597798791918');
+  this.load.image('tiles', 'https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2Fspritesheet_tiles.png?v=1597798793579');
   // Load the export Tiled JSON
   this.load.tilemapTiledJSON('map', 'https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2Ftest-map.tmx?v=1597798825593');
 }
