9feb88e32fd68537a2548ffc5e3a09175aed2390
Glitch (hello-webpage) <none>
2020-08-19 02:55:46 +0000

    💷🗼 Checkpoint ./.glitch-assets:11505919/783 ./game.js:11505919/786

--- game.js
+++ game.js
@@ -26,8 +26,10 @@ const game = new Phaser.Game(config);
   function preload() { 
   this.load.image('ground', 'https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2Fspritesheet_ground.png?v=1597798791918');
   this.load.image('tiles', 'https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2Fspritesheet_tiles.png?v=1597798793579');
+  this.load.image('background', ' https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2Fbackground.png?v=1597805558340');
+   
   // Load the export Tiled JSON
-  this.load.tilemapTiledJSON('map', 'https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2Ftest-map.tmx?v=1597798825593');
+  this.load.tilemapTiledJSON('map', 'https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2Ftest-map.json?v=1597805515636');
 }
 
 function create() { 
@@ -36,11 +38,11 @@ function create() {
   
   const map = this.make.tilemap({ key: 'map' });
   
-  const tileset = map.addTilesetImage('kenney_simple_platformer', 'tiles');
+  const tileset = map.addTilesetImage('spritesheet_ground', 'ground');
   
   const platforms = map.createStaticLayer('Platforms', tileset, 0, 200);
   
-  platforms.setScale(0.5, 0.5);
+  platforms.setScale(0.25, 0.25);
 }
 
 function update() { 
