084a127d7272b6606015a8cc65f94fdda9a8ba6f
Glitch (hello-webpage) <none>
2020-09-23 03:02:56 +0000

    🏪😀 Checkpoint ./game.js:1045018/311

--- game.js
+++ game.js
@@ -46,6 +46,7 @@ function preload() {
   this.load.image( "lavaWave", "https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2FlavaTop_high.png?v=1599615843450" );
   this.load.image( "bridge", "https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2FbridgeA.png?v=1600812709430");
   this.load.image( "chain", "https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2Fchain.png?v=1599615831446");
+  this.load.image( "cloud", "https://cdn.glitch.com/cd67e3a9-81c5-485d-bf8a-852d63395343%2Fsnow.png?v=1599615818886");
   
 
   
@@ -145,10 +146,10 @@ function create() {
   detailObjs.forEach(detailObject => {
     switch(detailObject.gid) {
       case 228:
-        let detail = this.badStuff.create(detailObject.x * 0.25, detailObject.y  * 0.25, 'chain').setScale(0.25, 0.25)
+         detail = this.badStuff.create(detailObject.x * 0.25, detailObject.y  * 0.25, 'chain').setScale(0.25, 0.25)
         break;
       case 131:
-        let detail = this.badStuff.create(detailObject.x * 0.25, detailObject.y  * 0.25, 'cloud').setScale(0.25, 0.25)
+         detail = this.badStuff.create(detailObject.x * 0.25, detailObject.y  * 0.25, 'cloud').setScale(0.25, 0.25)
         break;
       default:
         
